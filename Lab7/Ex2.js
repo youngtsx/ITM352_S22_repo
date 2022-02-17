27c107016217ba185c3a0eb7bbb5cfe2c137c719
youngtsx <tyoung24@hawaii.edu>
2022-02-17 11:58:23 -1000

    lab7

---
+++ Lab7/Ex2.js
@@ -0,0 +1,17 @@
+require("./products_data.js");
+
+var num_products = 5;
+let counter = 0
+while(counter < num_products) {
+    counter++;
+    if(counter > num_products/2){
+        console.log("Don’t ask for anything else!");
+        process.exit(); //break
+    }
+    if(counter > 0.25*num_products && counter < 0.75*num_products){
+        console.log(`Item #${counter} is sold out!`);
+        continue; //breaks one iteration in the loop and continues on
+}
+console.log(`${counter}. ${eval('name' + counter)}`);
+}
+console.log("That's all we have!")
