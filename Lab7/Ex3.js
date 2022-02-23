8a51e53f6b1335ff6ecff139b2bab5f0f4e83ff9
youngtsx <tyoung24@hawaii.edu>
2022-02-22 21:12:18 -1000

    lab7 finish

--- Lab7/Ex3.js
+++ Lab7/Ex3.js
@@ -7,7 +7,7 @@ for (counter = 1; counter <= num_products; counter++){
     counter++;
     if(counter > num_products/2){
         console.log("Don’t ask for anything else!");
-        process.exit(); //break
+        process.exit(); //exits node, doesn't work in browser
     }
     if(counter > 0.25*num_products && counter < 0.75*num_products){
         console.log(`Item #${counter} is sold out!`);
