090616beb88bd04b4d853a6b7ff626f2dc82c9dc
youngtsx <tyoung24@hawaii.edu>
2022-02-25 14:41:02 -1000

    smartphone 3

---
+++ WODs/SmartPhoneProducts2/products_data.js
@@ -0,0 +1,29 @@
+      product1={
+      "brand" : "HTC",
+      "price" : 40.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+      };
+      product2= {
+      "brand" : "Apple",
+      "price" : 75.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+      };
+      product3= {
+      "brand" : "Nokia",
+      "price" : 35.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+      };
+      product4= {
+      "brand" : "Samsung",
+      "price" : 45.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+      };
+      product5= {
+      "brand" : "Blackberry",
+      "price" : 10.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+      };
+
+     num_products = 5;
+
+     products=[product1, product2, product3, product4, product5];
