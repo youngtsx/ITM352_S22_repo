de0f6953ea4eca327fda21385873d9118ed329b6
youngtsx <tyoung24@hawaii.edu>
2022-03-17 23:48:05 -1000

    for loop products

--- Young_Tiffany_Assignment1/public/products.js
+++ Young_Tiffany_Assignment1/public/products.js
@@ -1,30 +1,30 @@
 var products = [
     {
-        "model": "Black Milk Tea",
+        "item": "Black Milk Tea",
         "price": 4.50,
         "image": "./images/blackmilktea.jpg",
         "quantity_available": 19
     },
     {
-        "model": "Mango Smoothie",
+        "item": "Mango Smoothie",
         "price": 4.99,
         "image": "./images/mango.jpg",
         "quantity_available": 4
     },
     {
-        "model": "Wintermelon Milk Tea",
+        "item": "Wintermelon Milk Tea",
         "price": 5.40,
         "image": "./images/wintermelon.jpeg",
         "quantity_available": 5
     },
     {
-        "model": "Brown Sugar Boba",
+        "item": "Brown Sugar Boba",
         "price": 5.50,
         "image": "./images/brownsugar.jpg",
         "quantity_available": 10
     },
     {
-        "model": "Thai Tea",
+        "item": "Thai Tea",
         "price": 5.49,
         "image": "./images/thaitea.jpg",
         "quantity_available": 14
