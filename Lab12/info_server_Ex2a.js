b9d1f6de3999f8640385dcd1d1618ff0e6b9e8a0
youngtsx <tyoung24@hawaii.edu>
2022-03-24 14:58:54 -1000

    in class

--- Lab12/info_server_Ex2a.js
+++ Lab12/info_server_Ex2a.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+//respond to any req for any path
 app.all('*', function (request, response, next) {
-    response.send(request.method + ' to path ' + request.path);
+    console.log(request.method + ' to path ' + request.path);
+    next();
 });
+app.get('/test', function (request, response, next) {
+    response.send(request.method + ' to paths ' + request.path);
+});
+app.post('/process_form', function (request, response, next) {
+    var q = request.body['quantity_textbox'];
+    if (typeof q != 'undefined') {
+    response.send(`Thank you for purchasing ${q} things!`);
+    } 
+});
+app.use(express.static(__dirname + '/public'));
+
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
