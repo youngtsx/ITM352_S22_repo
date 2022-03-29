e072a5cb5898f5a2af2416b18ec596d8d884de44
youngtsx <tyoung24@hawaii.edu>
2022-03-28 15:31:06 -1000

    done pt 2 lol

--- Young_Tiffany_Assignment1/server.js
+++ Young_Tiffany_Assignment1/server.js
@@ -2,7 +2,7 @@
 /*load product data*/ 
 var products= require(__dirname + '/products.json');
 
-products.forEach( (prod,i) => {prod.quantity_available = 52});
+products.forEach( (prod,i) => {prod.quantity_available = 15});
 var express = require('express');
 var app = express();
 
