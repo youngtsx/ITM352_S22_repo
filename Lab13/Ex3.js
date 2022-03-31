23691aa04af1c4a8a5047730be65eedc40c5189f
youngtsx <tyoung24@hawaii.edu>
2022-03-31 12:02:43 -1000

    Update Ex3.js

--- Lab13/Ex3.js
+++ Lab13/Ex3.js
@@ -7,9 +7,11 @@ var filename = 'user_data.json';
 if (fs.existsSync(filename)) {
     var data = fs.readFileSync(filename, 'utf-8');
     var user_data_obj = JSON.parse(data);
-    var file_stats = fs.statSync(filename);
+    console.log(user_data_obj);
 
+    var file_stats = fs.statSync(filename);
     console.log(`${filename} has ${file_stats.size} characters`);
+
 } else {
     console.log(`${filename} doesn't exist :(`);
 }
@@ -32,7 +34,20 @@ app.get("/login", function (request, response) {
 
 app.post("/login", function (request, response) {
     // Process login form POST and redirect to logged in page if ok, back to login page if not
+    let login_username = request.body['username'];
+    let login_password = request.body['password'];
 
+    if (typeof user_data_obj[login_username == 'undefined']) {
+        if (typeof user_data_obj[login_username]["password"] == login_password) {
+            response.send(`${login_username} is logged in`);
+        } else {
+        response.send(`Incorrect password for ${login_username}`)
+        } 
+    }
+    else {
+        response.send(`${login_username} does not exist`);
+    }
+    response.send('processing login', JSON.stringify(request.body));
 });
 
 app.listen(8080, () => console.log(`listening on port 8080`));
