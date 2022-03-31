8515b3fb9d1cfd27c794abdde072afda05d7a63b
youngtsx <tyoung24@hawaii.edu>
2022-03-31 13:15:45 -1000

    x

--- Lab13/Ex1.js
+++
@@ -1,2 +0,0 @@
-var user_reg_data = require('./user_data.json');
-console.log(user_reg_data/*['dport']['password']*/);
--- Lab13/Ex2a.js
+++ Lab13/Ex2a.js
@@ -5,7 +5,9 @@ var filename = 'user_data.json';
 if (fs.existsSync(filename)) {
     var data = fs.readFileSync(filename, 'utf-8');
     var user_data_obj = JSON.parse(data);
+    if (typeof user_data_obj["dport"] != 'undefined') {
     console.log(user_data_obj["dport"]["password"]);
+    }
 } else {
-    console.log("no file found :(");
+    console.log(`${filename} not found booo :(`);
 }
