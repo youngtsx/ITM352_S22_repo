720ebd7b65b6c4c8774e8cb6e28c8f9b3a495fee
youngtsx <tyoung24@hawaii.edu>
2022-04-07 19:55:52 -1000

    labwork

--- Lab14/Ex1.js
+++ Lab14/Ex1.js
@@ -1,7 +1,7 @@
-product = {name:'small gumball', price:'$0.34};
+product = {name:'small gumball', price: 0.34};
 
 tax_rate = 0.045;
 
 total = product.price + product.price * tax_rate;
 
-console.log(`A ${product.name} costs ${total}`); 
+console.log(`A ${product.name} costs \$${total}`);
--- Lab14/Ex4.js
+++ Lab14/Ex4.js
@@ -4,15 +4,15 @@ var app = express();
 
 app.get('/', function(req,res) {
     res.send(
-        '<form action="/process_form" method="POST">
+        `<form action="/process_form" method="POST">
             Name1: <input  name="name1"><br>
             Name2: <input  name="name1"><br>
             <input type="submit" name="Submit" value="Send POST Request">
-        </form>'
+        </form>`
         );
 });
 
-app.post('/processform', function(req, res) {
+app.post('/process_form', function(req, res) {
     if (typeof req.body['Submit'] != 'undefined') {
         for (value in req.body) {
             if (value == "Tyler") {
