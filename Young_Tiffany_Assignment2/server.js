aee4a423e0c5bf32b356c20ac5589e8e21921457
youngtsx <tyoung24@hawaii.edu>
2022-04-07 20:53:18 -1000

    Update server.js

--- Young_Tiffany_Assignment2/server.js
+++ Young_Tiffany_Assignment2/server.js
@@ -1,9 +1,11 @@
 /*michimoto momoka, reece nagaoka, lab 12 */
-/*load product data*/ 
-var products= require(__dirname + '/products.json');
+
+/*load product data*/
+var products = require(__dirname + '/products.json');
 var express = require('express');
 var app = express();
 var fs = require('fs')
+
 /* Initialize QueryString package */
 const qs = require('query-string');
 
@@ -25,70 +27,82 @@ if (fs.existsSync(filename)) {
 //get the body
 app.use(express.urlencoded({ extended: true }));
 
-/*LOGIN*/
+/*                 LOGIN                     */
 app.post("/process_login", function (request, response) {
-var errors = {};
+   var errors = {};
 
-//login form info from post
-var the_email = request.body['email'].toLowerCase();
-var the_password = request.body['password']
+   //login form info from post
+   var the_email = request.body['email'].toLowerCase();
+   var the_password = request.body['password']
 
-//check if username exists, then if entered password matches, lab 13 ex3-4
-if (typeof users[the_email] != 'undefined') {
-   //check if entered password matches the stored password
-   if (users[the_email].password == the_password) {
-      //matches
+   //check if username exists, then if entered password matches, lab 13 ex3-4
+   if (typeof users[the_email] != 'undefined') {
+      //check if entered password matches the stored password
+      if (users[the_email].password == the_password) {
+         //matches
 
-      //direct to invoice page **need to keep data
-      response.redirect('./invoice.html');
-      return;
+         //direct to invoice page **need to keep data
+         response.redirect('./invoice.html');
+         return;
+      } else {
+         //doesn't match
+         errors['login_err'] = "Wrong Password";
+      }
+
    } else {
-      //doesn't match
-      errors['login_err'] = "Wrong Password";
+      //email doesn't exist
+      errors['login_err'] = "Wrong Email";
    }
 
-} else {
-   //email doesn't exist
-   errors['login_err'] = "Wrong Email";
-}
-
-//redirect to login with error message
+   //redirect to login with error message
 
 
 
 });
 
-/*REGISTER USERS PAGE*/
+/*               REGISTER USERS PAGE                 */
 app.post("/register", function (request, response) {
-var registration_errors = {};
-//check email
-var reg_email = request.body['email'].toLowerCase();
+   var registration_errors = {};
+   //check email
+   var reg_email = request.body['email'].toLowerCase();
 
-//check email x@y.z
-/^[\w._]+@[\w]+\.[a-zA-Z]{2,3}$/
+   //check email x@y.z
+   if (/^[\w._]+@[\w]+\.[a-zA-Z]{2,3}$/.test(request.body.email) == false) {
+      registration_errors['email'] = 'Please enter a valid email';
+   }
 
-//check if email is unique
+   //check if email is unique
+   if (typeof users[reg_email] != 'undefined') {
+      registration_errors['email'] = 'This email has already been registered';
+   }
 
+   //check password >8 ******if this doesn't work, attempt to not use variable
+   if (request.body.password.length < 8) {
+      registration_errors['password'] = 'Minimum 8 characters';
+   };
 
-//check password >8 ******if this doesn't work, attempt to not use variable
-if (the_password.length < 8) {
-   registration_errors['password'] = 'Minimum 8 characters'
-};
+   //check repeated password for matches
+   if (request.body['password'] != request.body['repeat_password']) {
+      registration_errors['repeat_password'] = 'The passwords do not match';
+   }
 
-//check repeated password for matches
-if (request.body['password'] != request.body['repeat_password']){
-   registration_errors['repeat_password'] = `The passwords do not match`;
-}
+   //full name validation
+   if (/^[A-Za-z, ]+$/.test(request.body['fullname']) == false) { 
+      //check if the fullname is correct
+      registration_errors['fullname'] = 'Please enter your full name';
+   }
+   //check if fullname is less than 30 characters
+   if (request.body['fullname']) {
+      registration_errors['fullname'] = 'Please enter less than 30 characters';
+   }
 
-//full name validation
-if (/^[A-Za-z, ]+$/.test(req.body.fullname)) { //check if the fullname is correct
-}
-//check if less than 30 characters
-
-
-//save new registration data to user_data.json
+   //save new registration data to user_data.json
 
 });
+
+
+/*      Changing register users' data            */
+
 
 // Routing 
 app.get("/products.js", function (request, response, next) {
@@ -103,19 +117,19 @@ app.all('*', function (request, response, next) {
    next();
 });
 
-/*PURCHASE*/ 
+/*            PURCHASE              */
 // process purchase request (validate quantities, check quantity available)
-app.post('/process_form', function (request, response, next){
+app.post('/process_form', function (request, response, next) {
    var quantities = request.body['quantity'];
    //assume no errors or no quantity
    var errors = {};
    var check_quantities = false;
    //check for NonNegInt
    for (i in quantities) {
-      if (isNonNegInt(quantities[i]) == false){ //check i quantity
+      if (isNonNegInt(quantities[i]) == false) { //check i quantity
          errors['quantity_' + i] = `Please choose a valid quantity for ${products[i].item}.`;
       }
-      if (quantities[i] > 0 ) { //check if any quantity is selected
+      if (quantities[i] > 0) { //check if any quantity is selected
          check_quantities = true;
       }
       if (quantities[i] > products[i].quantity_available) { //check if quantity is available
@@ -126,22 +140,22 @@ app.post('/process_form', function (request, response, next){
       errors['no_quantities'] = `Please select a quantity`;
    }
 
-   let qty_obj = { "quantity": JSON.stringify(quantities)};
-    //ask if the object is empty or not
-    if (Object.keys(errors).length == 0) {
+   let qty_obj = { "quantity": JSON.stringify(quantities) };
+   //ask if the object is empty or not
+   if (Object.keys(errors).length == 0) {
       // remove quantities purchased from inventory quantities
-      for(i in products){
-          products[i].quantity_available -= Number(quantities[i]);
+      for (i in products) {
+         products[i].quantity_available -= Number(quantities[i]);
       }
       //save quantity data for invoice *****change this to redirect to ./login.html
-      //qty_data_obj = qty_obj;
+      qty_data_obj = qty_obj;
       response.redirect('./login.html');
-      } 
+   }
    else { //if i have errors, take the errors and go back to products_display.html
       let errs_obj = { "errors": JSON.stringify(errors) };
       console.log(qs.stringify(qty_obj));
       response.redirect('./shop.html?' + qs.stringify(qty_obj) + '&' + qs.stringify(errs_obj));
-  }
+   }
 });
 
 // route all other GET requests to files in public 
